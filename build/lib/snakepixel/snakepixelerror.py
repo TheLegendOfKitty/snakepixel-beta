34d2c1a71ab5e1f2b496a59af32b15db5b35d3d9
TheLegendOfKitty <61601745+TheLegendOfKitty@users.noreply.github.com>
2020-08-20 20:04:20 -0700

    Add files via upload

---
+++ build/lib/snakepixel/snakepixelerror.py
@@ -0,0 +1,14 @@
+class InvalidAPIKey(Exception):
+   pass
+class InvalidGuildName(Exception):
+    pass
+class InvalidUUID(Exception):
+    pass
+class InvalidGameType(Exception):
+    pass
+class InvalidGuildID(Exception):
+    pass
+class InvalidUsername(Exception):
+    pass
+class PageNumberOutOfRange(Exception):
+    pass
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="snakepixel-beta",
-    version="0.1.2",
+    version="0.1.4",
     author="TheLegendOfKitty",
     description="A python wrapper for the hypixel api",
     long_description=long_description,
